eeee030cc8c304f6798d146b7a01c0aef19b6f51
NZpatelK <patelk.dev@hotmail.com>
2024-12-31 12:45:33 +1300

    create menu anaimation

--- front-end/src/components/navbar/Navbar.tsx
+++ front-end/src/components/navbar/Navbar.tsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import map from '../../assets/map.png'
 import animals from '../../assets/livestock.png';
 
 export const Navbar: React.FC = () => {
+  const [isOpened, setIsOpened] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpened((prevState) => !prevState);
+  };
   return (
-    <div>
-      <nav className="navbar">
+    <div className={'navbar ' + (isOpened ? 'opened' : '')}>
+      {/* <nav className="navbar">
         <ul>
           <li>
             <img src={map} alt="" className='map-icon' />
@@ -15,7 +20,31 @@ export const Navbar: React.FC = () => {
             <img src={animals} alt="" className='animals-icon' />
           </li>
         </ul>
-      </nav>
+      </nav> */}
+      <div
+        className={`menu ${isOpened ? 'opened' : ''}`}
+        onClick={toggleMenu}
+        aria-label="Main Menu"
+        aria-expanded={isOpened}
+      >
+        <div className={"menu-icon"} >
+          <img src={map} alt="" className='map-icon' />
+          <img src={animals} alt="" className='animals-icon' />
+        </div>
+
+
+        <svg width="50" height="50" viewBox="0 0 100 100">
+          <path
+            className="line line1"
+            d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"
+          />
+          <path className="line line2" d="M 20,50 H 80" />
+          <path
+            className="line line3"
+            d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942"
+          />
+        </svg>
+        </div>
     </div>
   )
 }
