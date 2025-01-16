1ea0e2f7535a10e059ffaa9ecde6ab445fb1be68
NZpatelK <patelk.dev@hotmail.com>
2025-01-17 12:31:17 +1300

    add resusbale function in the dbhelpers

--- back-end/src/controllers/fieldController.js
+++ back-end/src/controllers/fieldController.js
@@ -1,47 +1,64 @@
 const fs = require('fs');
 const path = require('path');
 const fieldFilePath = path.join(__dirname, '../data/FieldData.json');
+const { getData, writeData } = require('../utils/dbHelpers');
 
 const getFields = async (req, res) => {
 
-    fs.readFile(fieldFilePath, 'utf-8', (err, data) => {
-        if (err) {
-            return res.status(500).send('Error reading the data file');
-        }
+    // fs.readFile(fieldFilePath, 'utf-8', (err, data) => {
+    //     if (err) {
+    //         return res.status(500).send('Error reading the data file');
+    //     }
 
-        res.json(JSON.parse(data));
-    });
+    //     res.json(JSON.parse(data));
+    // });
+
+    try {
+        const fetchData = await getData(fieldFilePath);
+        res.json(fetchData);
+    } catch (error) {
+        handleError(res, error); 
+    }
 };
 
 const addField = async (req, res) => {
     const data = req.body; // Get data from the request body
 
     // Read the existing data from the JSON file
-    fs.readFile(fieldFilePath, 'utf8', (err, jsonData) => {
-        if (err) {
-            return res.status(500).json({ message: 'Error reading data file' });
-        }
+    // fs.readFile(fieldFilePath, 'utf8', (err, jsonData) => {
+    //     if (err) {
+    //         return res.status(500).json({ message: 'Error reading data file' });
+    //     }
 
-        let currentData = [];
+    //     let currentData = [];
 
-        try {
-            currentData = JSON.parse(jsonData);
-            if (!Array.isArray(currentData)) {
-                currentData = [];
-            }
-        } catch (parseError) {
-            return res.status(500).json({ message: 'Error parsing data file' });
-        }
+    //     try {
+    //         currentData = JSON.parse(jsonData);
+    //         if (!Array.isArray(currentData)) {
+    //             currentData = [];
+    //         }
+    //     } catch (parseError) {
+    //         return res.status(500).json({ message: 'Error parsing data file' });
+    //     }
 
+    //     currentData.push(data);
+
+    //     fs.writeFile(fieldFilePath, JSON.stringify(currentData, null, 2), (err) => {
+    //         if (err) {
+    //             return res.status(500).json({ message: 'Error saving data' });
+    //         }
+    //         res.status(200).json({ message: 'Data saved successfully' });
+    //     });
+    // });
+
+    try {
+        const currentData = await getData(fieldFilePath);
         currentData.push(data);
-
-        fs.writeFile(fieldFilePath, JSON.stringify(currentData, null, 2), (err) => {
-            if (err) {
-                return res.status(500).json({ message: 'Error saving data' });
-            }
-            res.status(200).json({ message: 'Data saved successfully' });
-        });
-    });
+        await writeData(fieldFilePath, currentData);
+        res.status(200).json({ message: 'Data saved successfully' });
+    } catch (error) {
+        handleError(res, error); 
+    }
 };
 
 module.exports = {
