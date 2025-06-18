2826242046a447f83df8c36facd87f5517f81dfb
NZpatelK <patelk.dev@hotmail.com>
2025-06-18 19:07:42 +1200

    fix the ui style for pasture modal

--- front-end/src/components/display-map/DisplayPastures.tsx
+++ front-end/src/components/display-map/DisplayPastures.tsx
@@ -21,20 +21,26 @@ export default function DisplayPastures({ pastures, handleClickRecenter, setSele
     return (
         <>
             {pastures.map((pasture) => (
-                <div key={pasture.id} className="pasture-item" onClick={() => handleClickRecenter(pasture.id as string, "pasture")}>
+                <div key={pasture.id} className="pasture-item">
                     <h3>{pasture.name}</h3>
 
                     {AnimalUtils.getAnimals() &&
-                        <div>
+                        <div className="animal-counts">
                             <p>Total Animals: {AnimalUtils.getAnimalsByPastureId(pasture.id).length}</p>
+                            <p className="animal-type">More Info</p>
 
-                            <div className="animal-counts">
+                            {/* <div className="animal-counts">
                                 <p>Pig: {AnimalUtils.getAnimalsByPastureId(pasture.id).filter((animal) => animal.type === "Pig").length}</p>
                                 <p>Goat: {AnimalUtils.getAnimalsByPastureId(pasture.id).filter((animal) => animal.type === "Goat").length}</p>
                                 <p>Sheep: {AnimalUtils.getAnimalsByPastureId(pasture.id).filter((animal) => animal.type === "Sheep").length}</p>
                                 <p>Cow: {AnimalUtils.getAnimalsByPastureId(pasture.id).filter((animal) => animal.type === "Cow").length}</p>
-                            </div>
+                            </div> */}
                         </div>}
+
+                        <p>Health Alert:</p>
+                        <p>Glazing Status:</p>
+                        <p>Pasture Size</p>
+                        <button className="recentre-pasture" onClick={() => handleClickRecenter(pasture.id as string, "pasture")}>Recenter Pasture</button>
 
                 </div>))}
             <button className='pasture-btn' onClick={() => { setSelectedOption("pasture-edit") }} disabled={selectedOption === "pasture-edit"}>Edit Pasture</button>
